a11350bd4c209bc6202265221ddc99449473ad13
xanoosh <xanoosh@gmail.com>
2023-02-26 13:21:09 +0100

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import { pokemonArray } from './data';
+import Fuse from 'fuse.js';
+const fuse = new Fuse(pokemonArray, {
+  keys: ['name'],
+  distance: 1,
+  minMatchCharLength: 2,
+});
+
+function App() {
+  const [searchValue, setSearchValue] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  function handleFuseSearch(searchVal) {
+    const results = fuse.search(searchVal);
+    setSearchResults(results);
+    console.log(results);
+  }
+
+  return (
+    <div className="App">
+      <h1>Stat checker</h1>
+      <div className="card">
+        <input
+          type="text"
+          value={searchValue}
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+            handleFuseSearch(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        {searchResults.length > 0 ? (
+          searchResults.map((searchResult) => <p>{searchResult.item.name}</p>)
+        ) : (
+          <p className="read-the-docs">No results.</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
