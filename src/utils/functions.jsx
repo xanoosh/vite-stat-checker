f35c0fb6959cf4e03d728952ef381336612d5bf9
xanoosh <xanoosh@gmail.com>
2023-02-28 22:10:55 +0100

    local storage data caching

--- src/utils/functions.jsx
+++ src/utils/functions.jsx
@@ -1,12 +1,17 @@
 const handleApiCall = (pokeId, setPokeData) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
-    .then((response) => {
-      if (!response.ok) console.error(response.error);
-      return response.json();
-    })
-    .then((result) => {
-      setPokeData(result);
-    });
+  if (localStorage.getItem(pokeId)) {
+    setPokeData(JSON.parse(localStorage.getItem(pokeId)));
+  } else {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
+      .then((response) => {
+        if (!response.ok) console.error(response.error);
+        return response.json();
+      })
+      .then((result) => {
+        setPokeData(result);
+        localStorage.setItem(pokeId, JSON.stringify(result));
+      });
+  }
 };
 
 export { handleApiCall };
