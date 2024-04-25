99fc2b560317a6eab558a0a993a627193c7ad590
xanoosh <xanoosh@gmail.com>
2024-04-25 15:38:00 +0200

    select item styling, icon added

--- src/components/responseColumn/SelectComponent.jsx
+++ src/components/responseColumn/SelectComponent.jsx
@@ -1,5 +1,6 @@
 import * as Select from '@radix-ui/react-select';
 import '../../styles/select.css';
+import { ChevronDownIcon } from '@radix-ui/react-icons';
 
 export default function SelectComponent({ items, value, onValueChange }) {
   if (items.length === 0) return null;
@@ -8,15 +9,19 @@ export default function SelectComponent({ items, value, onValueChange }) {
       <Select.Trigger className="SelectTrigger">
         <Select.Value>{value}</Select.Value>
         <Select.Icon className="SelectIcon">
-          {/* <ChevronDownIcon /> */}x
+          <ChevronDownIcon />
         </Select.Icon>
       </Select.Trigger>
       <Select.Portal>
-        <Select.Content className="SelectContent">
+        <Select.Content
+          className="SelectContent"
+          sideOffset={3}
+          position="popper"
+        >
           <Select.Viewport className="SelectViewport">
             {items.map((item, index) => (
-              <Select.Item key={index} value={item}>
-                <Select.ItemText>item</Select.ItemText>
+              <Select.Item className="SelectItem" key={index} value={item}>
+                <Select.ItemText>{item}</Select.ItemText>
               </Select.Item>
             ))}
             <Select.Separator />
