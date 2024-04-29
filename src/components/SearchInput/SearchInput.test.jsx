ed621fc456cd7280d50e5e4e848c95d245d941a2
xanoosh <xanoosh@gmail.com>
2024-04-29 10:25:50 +0200

    depenedencies update, working test example added

--- src/components/SearchInput/SearchInput.test.jsx
+++ src/components/SearchInput/SearchInput.test.jsx
@@ -1,10 +1,17 @@
 import { render, screen } from '@testing-library/react';
-import SearchInput from './SearchInput';
-import { describe, it, expect } from 'vitest';
+import SearchInput from './SearchInput.jsx';
+import { describe, it, expect, vi } from 'vitest';
 
 describe('SearchInput Component', () => {
-  it('Should be visible', () => {
-    render(<SearchInput handleSearch={() => null} />);
-    expect(screen.getAllByRole('text').toBeInTheDocument());
+  it('should be visible', () => {
+    const { container } = render(<SearchInput handleSearch={() => null} />);
+    expect(container.querySelector('input').toBeInTheDocument);
+  });
+  it('handleSearch should not be called on mount', () => {
+    const mockFunction = vi.fn();
+
+    render(<SearchInput handleSearch={mockFunction} />);
+    expect(mockFunction.mock.calls.length === 0);
+    // expect(mockFunction.toHaveBeenCalledTimes(0));
   });
 });
